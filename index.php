e6ec617b98a639262d194c9459d8b0405276032e
ZacharyTam <zacharytam@ZacharyTams-Mac-mini.local>
2021-07-13 02:04:32 +0800

    y

--- index.php
+++ index.php
@@ -204,7 +204,6 @@ $(function () {
                     var text = $(this).val();
                     $(cell).html(text);
                     editTableObj.cell(cell).data(text);
-                    editTableObj.cell(cell).draw(text);
                 })
             }
         }, {
