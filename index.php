7cf0b6e72bbbd5a3498e3f5c25a26a8a3fb787bc
ZacharyTam <zacharytam@ZacharyTams-Mac-mini.local>
2021-07-12 17:25:22 +0800

    y

--- index.php
+++ index.php
@@ -17,6 +17,14 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <script src="https://markcell.github.io/jquery-tabledit/assets/js/tabledit.min.js"></script>-->
 
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://markcell.github.io/jquery-tabledit/assets/js/tabledit.min.js"></script>
+
     <style>
         body,
         html {
