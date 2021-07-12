5f4bdfb01aafaaf0e202934368d2929c7694ed04
ZacharyTam <zacharytam@ZacharyTams-Mac-mini.local>
2021-07-12 18:13:26 +0800

    y

--- index.php
+++ index.php
@@ -197,7 +197,7 @@ $(function () {
             { "data": "note" }
         ],
         columnDefs: [{
-            "targets": [0, 1],
+            "targets": [7],
             createdCell: function (cell, cellData, rowData, rowIndex, colIndex) {
                 $(cell).click(function () {
                     $(this).html('<input type="text" size="16" style="width: 100%"/>');
@@ -211,7 +211,7 @@ $(function () {
                 })
             }
         }, {
-            "targets": [2, 3, 4],
+            "targets": [],
             createdCell: function (cell, cellData, rowData, rowIndex, colIndex) {
                 var aInput;
                 $(cell).click(function () {
