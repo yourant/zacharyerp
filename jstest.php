29a9f0145fdd2ea6e7320f6e3ebc949a89c06ff6
ZacharyTam <zacharytam@ZacharyTams-Mac-mini.local>
2021-07-12 17:31:42 +0800

    y

---
+++ jstest.php
@@ -0,0 +1,387 @@
+<link rel="stylesheet" href="./bootstrap.min.css">
+<script src="http://code.jquery.com/jquery-2.2.1.min.js"></script>
+
+<?php
+
+class MySqli_DB {
+
+	private $con;
+	public $query_id;
+
+	public function db_connect()
+	{
+		$this->con = mysqli_connect("127.0.0.1","test","nr4oAX_F5e4jpif5d");
+		if(!$this->con)
+		{
+			die(" Database connection failed:". mysqli_connect_error());
+		} else {
+			$select_db = $this->con->select_db('unfulfilled');
+	 		if(!$select_db){
+				die("Failed to Select Database". mysqli_connect_error());
+			}
+		}
+	}
+
+	public function query($sql)
+   {
+
+		if (trim($sql != "")) {
+			$this->query_id = $this->con->query($sql);
+		}
+        // only for Develope mode
+       // For production mode
+        //  die("Error on Query");
+       return $this->query_id;
+
+   }
+
+	public function createTable($DBorder)
+	{
+		$sql = "CREATE TABLE order". $DBorder ." (
+	    created_at VARCHAR(30),
+	    title VARCHAR(200),
+	    variant VARCHAR(30),
+	    sku VARCHAR(40),
+	    quantity INT
+		)";
+		if(mysqli_query($this->con, $sql)){
+		    echo "order". $DBorder ."Table created successfully.";
+		} else{
+		    echo "ERROR: Could not able to execute $sql. ";
+		}
+	}
+
+	public function updateTable($DBorder,$DBcreated_at,$DBtitle,$DBvariant,$DBsku,$DBquantity)
+	{
+
+	}
+
+	public function db_disconnect()
+	{
+		if(isset($this->con))
+		{
+		mysqli_close($this->con);
+		unset($this->con);
+		}
+	}
+}
+
+
+//https://delladirect.myshopify.com/admin/orders.json?limit=250&selectedView=savedSearch&status=open&fulfillment_status=unfulfilled&order=processed_at%20asc&processed_at_min=2021-01-01&processed_at_max=2021-06-24
+
+//$url = "https://2690fb974069f84abb68de4c26972018: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@she-sho-wholesale.myshopify.com/admin/orders.json";
+$db = new MySqli_DB();
+$db->db_connect();
+
+$Lineitems = [];
+$created_at = "";
+$title = "";
+$variant = "";
+$sku = "";
+$quantity = 0;
+$skuArray = [];
+$table_array = [];
+
+$query_orders = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'unfulfilled'";
+$tables = $db->query($query_orders);
+foreach ($tables as $table) {
+	$tableName = $table["table_name"];
+	//$query_arribute = "SELECT created_at, title, variant, sku, quantity FROM " . $tableName;
+	$result = $db->query("SELECT created_at, title, variant, sku, quantity FROM " . $tableName);
+	//print_r($orderNumber);
+	foreach ($result as $row) {
+		array_push($table_array,array('order' => $tableName, 'created_at' => $row["created_at"], 'title' => $row["title"], 'variant' => $row["variant"], 'sku' => $row["sku"], 'quantity' => $row["quantity"], 'note' => $row["note"]));
+		/*echo "<tr>
+		<td>". $tableName . "</td>
+		<td>". $row["created_at"] . "</td>
+		<td>". $row["title"] . "</td>
+		<td>". $row["variant"] . "</td>
+		<td>". $row["sku"] . "</td>
+		<td>". $row["quantity"] . "</td>
+		</tr>";*/
+	}
+	/*while($row = $result->fetch_assoc()) {
+		print_r("3");
+		echo "<tr>
+		<td>". $orderNumber . "</td>
+		<td>". $row["created_at"] . "</td>
+		<td>". $row["title"] . "</td>
+		<td>". $row["variant"] . "</td>
+		<td>". $row["sku"] . "</td>
+		<td>". $row["quantity"] . "</td>
+		</tr>";
+	}*/
+}
+
+$db->db_disconnect();
+
+?>
+ 
+
+
+<script type="text/javascript">
+$(document).ready(function($)
+{
+	//ajax row data
+	var ajax_data = <?php echo json_encode($table_array); ?>;
+	/*[
+		{fname:"Code", lname:"With Mark", email:"mark@codewithmark.com"}, 
+		{fname:"Mary", lname:"Moe", email:"mary@gmail.com"},
+		{fname:"John", lname:"Doe", email:"john@yahoo.com"},
+		{fname:"Julie", lname:"Dooley", email:"julie@gmail.com"},
+	]*/
+
+
+	console.log("123");
+	var random_id = function  () 
+	{
+		var id_num = Math.random().toString(9).substr(2,3);
+		var id_str = Math.random().toString(36).substr(2);
+		
+		return id_num + id_str;
+	}
+
+
+	//--->create data table > start
+	var tbl = '';
+	tbl +='<table class="table table-hover">'
+
+		//--->create table header > start
+		tbl +='<thead>';
+			tbl +='<tr>';
+			tbl +='<th>Order</th>';
+			tbl +='<th>Created_at</th>';
+			tbl +='<th>Product Name</th>';
+			tbl +='<th>Variant</th>';
+			tbl +='<th>SKU</th>';
+			tbl +='<th>Quantity</th>';
+			tbl +='<th>Note</th>';
+			tbl +='</tr>';
+		tbl +='</thead>';
+		//--->create table header > end
+
+		
+		//--->create table body > start
+		tbl +='<tbody>';
+
+			//--->create table body rows > start
+			$.each(ajax_data, function(index, val) 
+			{
+				//you can replace with your database row id
+				var row_id = random_id();
+
+				//loop through ajax row data
+				tbl +='<tr row_id="'+row_id+'">';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="order">'+val['order']+'</div></td>';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="created_at">'+val['created_at']+'</div></td>';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="title">'+val['title']+'</div></td>';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="variant">'+val['variant']+'</div></td>';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="sku">'+val['sku']+'</div></td>';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="quantity">'+val['quantity']+'</div></td>';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="note">'+val['note']+'</div></td>';
+
+					//--->edit options > start
+					tbl +='<td>';
+					 
+						tbl +='<span class="btn_edit" > <a href="#" class="btn btn-link " row_id="'+row_id+'" > Edit</a> </span>';
+
+						//only show this button if edit button is clicked
+						tbl +='<span class="btn_save"> <a href="#" class="btn btn-link"  row_id="'+row_id+'"> Save</a> | </span>';
+						tbl +='<span class="btn_cancel"> <a href="#" class="btn btn-link" row_id="'+row_id+'"> Cancel</a> | </span>';
+
+					tbl +='</td>';
+					//--->edit options > end
+					
+				tbl +='</tr>';
+			});
+
+			//--->create table body rows > end
+
+		tbl +='</tbody>';
+		//--->create table body > end
+
+	tbl +='</table>'	
+	//--->create data table > end
+
+	//out put table data
+	$(document).find('.tbl_user_data').html(tbl);
+
+	$(document).find('.btn_save').hide();
+	$(document).find('.btn_cancel').hide();
+	console.log("1234");
+
+
+	//--->make div editable > start
+	$(document).on('click', '.row_data', function(event) 
+	{
+		event.preventDefault(); 
+
+		if($(this).attr('edit_type') == 'button')
+		{
+			return false; 
+		}
+
+		//make div editable
+		$(this).closest('div').attr('contenteditable', 'true');
+		//add bg css
+		$(this).addClass('bg-warning').css('padding','5px');
+
+		$(this).focus();
+	})	
+	//--->make div editable > end
+
+
+	//--->save single field data > start
+	$(document).on('focusout', '.row_data', function(event) 
+	{
+		event.preventDefault();
+
+		if($(this).attr('edit_type') == 'button')
+		{
+			return false; 
+		}
+
+		var row_id = $(this).closest('tr').attr('row_id'); 
+		
+		var row_div = $(this)				
+		.removeClass('bg-warning') //add bg css
+		.css('padding','')
+
+		var col_name = row_div.attr('col_name'); 
+		var col_val = row_div.html(); 
+
+		var arr = {};
+		arr[col_name] = col_val;
+
+		//use the "arr"	object for your ajax call
+		$.extend(arr, {row_id:row_id});
+
+		//out put to show
+		$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>');
+		
+	})	
+	//--->save single field data > end
+
+ 
+	//--->button > edit > start	
+	$(document).on('click', '.btn_edit', function(event) 
+	{
+		event.preventDefault();
+		var tbl_row = $(this).closest('tr');
+
+		var row_id = tbl_row.attr('row_id');
+
+		tbl_row.find('.btn_save').show();
+		tbl_row.find('.btn_cancel').show();
+
+		//hide edit button
+		tbl_row.find('.btn_edit').hide(); 
+
+		//make the whole row editable
+		tbl_row.find('.row_data')
+		.attr('contenteditable', 'true')
+		.attr('edit_type', 'button')
+		.addClass('bg-warning')
+		.css('padding','3px')
+
+		//--->add the original entry > start
+		tbl_row.find('.row_data').each(function(index, val) 
+		{  
+			//this will help in case user decided to click on cancel button
+			$(this).attr('original_entry', $(this).html());
+		}); 		
+		//--->add the original entry > end
+
+	});
+	//--->button > edit > end
+
+
+	//--->button > cancel > start	
+	$(document).on('click', '.btn_cancel', function(event) 
+	{
+		event.preventDefault();
+
+		var tbl_row = $(this).closest('tr');
+
+		var row_id = tbl_row.attr('row_id');
+
+		//hide save and cacel buttons
+		tbl_row.find('.btn_save').hide();
+		tbl_row.find('.btn_cancel').hide();
+
+		//show edit button
+		tbl_row.find('.btn_edit').show();
+
+		//make the whole row editable
+		tbl_row.find('.row_data')
+		.attr('edit_type', 'click')
+		.removeClass('bg-warning')
+		.css('padding','') 
+
+		tbl_row.find('.row_data').each(function(index, val) 
+		{   
+			$(this).html( $(this).attr('original_entry') ); 
+		});  
+	});
+	//--->button > cancel > end
+
+	
+	//--->save whole row entery > start	
+	$(document).on('click', '.btn_save', function(event) 
+	{
+		event.preventDefault();
+		var tbl_row = $(this).closest('tr');
+
+		var row_id = tbl_row.attr('row_id');
+
+		
+		//hide save and cacel buttons
+		tbl_row.find('.btn_save').hide();
+		tbl_row.find('.btn_cancel').hide();
+
+		//show edit button
+		tbl_row.find('.btn_edit').show();
+
+
+		//make the whole row editable
+		tbl_row.find('.row_data')
+		.attr('edit_type', 'click')
+		.removeClass('bg-warning')
+		.css('padding','') 
+
+		//--->get row data > start
+		var arr = {}; 
+		tbl_row.find('.row_data').each(function(index, val) 
+		{   
+			var col_name = $(this).attr('col_name');  
+			var col_val  =  $(this).html();
+			arr[col_name] = col_val;
+		});
+		//--->get row data > end
+
+		//use the "arr"	object for your ajax call
+		$.extend(arr, {row_id:row_id});
+
+		//out put to show
+		$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>')
+		 
+
+	});
+	//--->save whole row entery > end
+
+
+}); 
+</script>
+
+ 
+
+<div class="panel panel-default">
+  <div class="panel-heading"><b> Unfulfilled Items </b> </div>
+
+  <div class="panel-body">
+	
+	<div class="tbl_user_data"></div>
+
+  </div>
+
+</div>
